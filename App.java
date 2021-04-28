a5a4149b0e3489736fb0ba9121be4008101d552e
dougdalo <43898653+dougdalo@users.noreply.github.com>
2021-04-27 21:15:31 -0300

    AV1.Tads

---
+++ App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class App {
+   
+    public static void main(String[] args) throws Exception {
+        boolean i;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println(" Digite um numero de 0 a 99");
+        i = entrada.nextBoolean();
+    if  (i = < 0 || i > 99 ) {
+        System.out.println("O numero digitado e: " + i);
+     }else (i > 0 || i < 99 ){
+        System.out.println("Numero Invalido");
+     }
+    }
+}
